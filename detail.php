6414c4cb9055744164a0772f151d52cb78476ece
EXXUVIA <hanan7taqiyya@gmail.com>
2026-07-15 18:46:00 +0700

    style: update login button layout to use flexbox for better alignment

--- detail.php
+++ detail.php
@@ -97,7 +97,7 @@ require_once 'templates/header.php';
                                 </button>
                             </form>
                         <?php else: ?>
-                            <a href="auth/login.php" class="btn-neo btn-neo-lg bg-yellow text-black detail-action-button" style="text-align: center; display: block;">
+                            <a href="auth/login.php" class="btn-neo btn-neo-lg bg-yellow text-black detail-action-button" style="display: flex; align-items: center; justify-content: center; width: 100%; font-size: 1.125rem;">
                                 Login untuk Membeli <i class="fa-solid fa-arrow-right" style="margin-left: 0.5rem;"></i>
                             </a>
                         <?php endif; ?>
